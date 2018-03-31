993608458ac7c94b0024b437c10a2604b44bdab7
Ben Webb <ben@salilab.org>
2018-03-31 00:02:11 -0700

    Update for addition of struct.entry_id

--- test/test_examples.py
+++ test/test_examples.py
@@ -25,7 +25,7 @@ class Tests(unittest.TestCase):
             # Make sure that a complete output file was produced
             with open(os.path.join(tmpdir, 'output.cif')) as fh:
                 contents = fh.readlines()
-            self.assertEqual(len(contents), 262)
+            self.assertEqual(len(contents), 263)
 
     def test_locations_example(self):
         """Test locations example"""
@@ -36,7 +36,7 @@ class Tests(unittest.TestCase):
         # Make sure that a complete output file was produced
         with open(out) as fh:
             contents = fh.readlines()
-        self.assertEqual(len(contents), 62)
+        self.assertEqual(len(contents), 63)
         os.unlink(out)
 
 
