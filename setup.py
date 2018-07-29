85598a4bd47071a8d7fc5804d8214bff48458749
Ben Webb <ben@salilab.org>
2018-07-28 22:33:43 -0700

    Allow building without the C extension

--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@
 
 from distutils.core import setup, Extension
 import subprocess
+import sys
+
+copy_args = sys.argv[1:]
+
+# Allow building without the C extension
+build_ext = True
+if '--without-ext' in copy_args:
+    build_ext = False
+    copy_args.remove('--without-ext')
 
 def getoutput(args):
     return subprocess.check_output(args, universal_newlines=True)
@@ -14,21 +23,25 @@ def pkgconfig(*packages, **kw):
         kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
     return kw
 
-# Get paths for glib 2.0:
-glib = pkgconfig("glib-2.0")
+if build_ext:
+    # Get paths for glib 2.0:
+    glib = pkgconfig("glib-2.0")
 
-mod = Extension("ihm._reader",
-                sources=["src/reader.c", "src/reader.i"],
-                include_dirs=glib['include_dirs'],
-                library_dirs=glib.get('library_dirs', []),
-                libraries=glib['libraries'],
-                swig_opts=['-keyword', '-nodefaultctor',
-                           '-nodefaultdtor', '-noproxy'])
+    mod = [Extension("ihm._reader",
+                     sources=["src/reader.c", "src/reader.i"],
+                     include_dirs=glib['include_dirs'],
+                     library_dirs=glib.get('library_dirs', []),
+                     libraries=glib['libraries'],
+                     swig_opts=['-keyword', '-nodefaultctor',
+                                '-nodefaultdtor', '-noproxy'])]
+else:
+    mod = []
 
 setup(name='ihm',
+      script_args=copy_args,
       description='Package for handling IHM mmCIF files',
       author='Ben Webb',
       author_email='ben@salilab.org',
       url='https://github.com/ihmwg/python-ihm',
-      ext_modules=[mod],
+      ext_modules=mod,
       packages=['ihm'])
