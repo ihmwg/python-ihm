ab4130616d2c7c636dab0d0d84104a128d838125
Ben Webb <ben@salilab.org>
2018-07-28 20:15:23 -0700

    Add distutils build recipe for C parser

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Extension
+import subprocess
+
+def getoutput(args):
+    return subprocess.check_output(args, universal_newlines=True)
+
+def pkgconfig(*packages, **kw):
+    """Utility function to parse pkg-config output"""
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    for token in getoutput(["pkg-config", "--libs",
+                            "--cflags"] + list(packages)).split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
+
+# Get paths for glib 2.0:
+glib = pkgconfig("glib-2.0")
+
+mod = Extension("ihm._reader",
+                sources=["src/reader.c", "src/reader.i"],
+                include_dirs=glib['include_dirs'],
+                library_dirs=glib.get('library_dirs', []),
+                libraries=glib['libraries'],
+                swig_opts=['-keyword', '-nodefaultctor',
+                           '-nodefaultdtor', '-noproxy'])
 
 setup(name='ihm',
       description='Package for handling IHM mmCIF files',
       author='Ben Webb',
       author_email='ben@salilab.org',
       url='https://github.com/ihmwg/python-ihm',
+      ext_modules=[mod],
       packages=['ihm'])
