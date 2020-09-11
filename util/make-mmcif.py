e80362c9381d6260d4ab49c611c35943e8fc6ecf
Ben Webb <ben@salilab.org>
2020-09-11 14:30:50 -0700

    Support input containing multiple models

--- util/make-mmcif.py
+++ util/make-mmcif.py
@@ -14,33 +14,47 @@ def get_input_parser(fname):
         return Bio.PDB.PDBParser()
 
 
-class PDBModel(ihm.model.Model):
-    """Pass a BioPython model through to IHM"""
+class AllPDBModels(object):
+    """Pass all BioPython models through to IHM"""
     def __init__(self, file_name, asym_units, chem_comps, **kwargs):
-        super(PDBModel, self).__init__(**kwargs)
+        self.kwargs = kwargs
         self.file_name = file_name
         self.asym_units = asym_units
         self.chem_comps = chem_comps
 
-    def get_atoms(self):
-        # Use BioPython to read the structure from a PDB file, and then yield
-        # a set of ihm.model.Atom objects
+    def get_models(self):
+        # Use BioPython to read the structure from a PDB file, and yield
+        # a PDBModel for each model in the file
         p = get_input_parser(self.file_name)
         s = p.get_structure('rep', self.file_name)
         for model in s:
-            for nchain, chain in enumerate(model):
-                asym = self.asym_units[nchain]
-                nres = 1
-                for residue in chain:
-                    # Skip unknown residues, non-polymers
-                    if residue.resname not in self.chem_comps:
-                        continue
-                    for atom in residue:
-                        coord = atom.get_vector()
-                        yield ihm.model.Atom(asym_unit=asym, seq_id=nres,
-                                atom_id=atom.get_id(), x=coord[0], y=coord[1],
-                                z=coord[2], type_symbol=atom.element)
-                    nres += 1
+            yield PDBModel(bp_model=model, asym_units=self.asym_units,
+                           chem_comps=self.chem_comps, **self.kwargs)
+
+
+class PDBModel(ihm.model.Model):
+    """Pass a BioPython model through to IHM"""
+    def __init__(self, bp_model, asym_units, chem_comps, **kwargs):
+        super(PDBModel, self).__init__(**kwargs)
+        self.bp_model = bp_model
+        self.asym_units = asym_units
+        self.chem_comps = chem_comps
+
+    def get_atoms(self):
+        # Yield a set of ihm.model.Atom objects for a BioPython model
+        for nchain, chain in enumerate(self.bp_model):
+            asym = self.asym_units[nchain]
+            nres = 1
+            for residue in chain:
+                # Skip unknown residues, non-polymers
+                if residue.resname not in self.chem_comps:
+                    continue
+                for atom in residue:
+                    coord = atom.get_vector()
+                    yield ihm.model.Atom(asym_unit=asym, seq_id=nres,
+                            atom_id=atom.get_id(), x=coord[0], y=coord[1],
+                            z=coord[2], type_symbol=atom.element)
+                nres += 1
 
 
 def get_sequence(fname, chem_comps):
@@ -49,7 +63,8 @@ def get_sequence(fname, chem_comps):
     s = p.get_structure('rep', fname)
     seqs = []
     chains = []
-    for chain in s.get_chains():
+    model0 = next(s.get_models())
+    for chain in model0.get_chains():
         chains.append(chain.id)
         seq = []
         for r in chain.get_residues():
@@ -82,12 +97,13 @@ system = ihm.System(title='Auto-generated system')
 # Make asym units for each chain and entities for all unique sequences
 seen_seqs = {}
 for seq, chain in zip(seqs, chains):
-    if tuple(seq) in seen_seqs:
-        entity = seen_seqs[seq]
+    tupseq = tuple(seq)
+    if tupseq in seen_seqs:
+        entity = seen_seqs[tupseq]
     else:
         entity = ihm.Entity(seq)
         system.entities.append(entity)
-        seen_seqs[tuple(seq)] = entity
+        seen_seqs[tupseq] = entity
     asym = ihm.AsymUnit(entity, id=chain)
     system.asym_units.append(asym)
 
@@ -102,11 +118,11 @@ rep = ihm.representation.Representation(
 # Simple modeling protocol
 protocol = ihm.protocol.Protocol(name='modeling')
 
-# Single state in a single state group, containing a single model
-m = PDBModel(assembly=assembly, representation=rep, file_name=fname,
-             asym_units=system.asym_units, chem_comps=chem_comps,
-             protocol=protocol)
-s = ihm.model.State([ihm.model.ModelGroup([m])])
+# Single state in a single state group, containing all models in the file
+allm = AllPDBModels(assembly=assembly, representation=rep, file_name=fname,
+                    asym_units=system.asym_units, chem_comps=chem_comps,
+                    protocol=protocol)
+s = ihm.model.State([ihm.model.ModelGroup(allm.get_models())])
 sg = ihm.model.StateGroup([s])
 system.state_groups.append(sg)
 
