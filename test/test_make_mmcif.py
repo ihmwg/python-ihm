201b462ae663670b5c00de21e3ad86baba5b41de
Ben Webb <ben@salilab.org>
2022-08-03 10:46:08 -0700

    Add basic test of make-mmcif.py script

---
+++ test/test_make_mmcif.py
@@ -0,0 +1,23 @@
+import utils
+import os
+import sys
+import unittest
+import subprocess
+
+TOPDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+utils.set_search_paths(TOPDIR)
+
+
+MAKE_MMCIF = os.path.join(TOPDIR, 'util', 'make-mmcif.py')
+
+
+class Tests(unittest.TestCase):
+    def test_simple(self):
+        """Simple test of make-mmcif utility script"""
+        incif = utils.get_input_file_name(TOPDIR, 'struct_only.cif')
+        subprocess.check_call([sys.executable, MAKE_MMCIF, incif])
+        os.unlink('output.cif')
+
+
+if __name__ == '__main__':
+    unittest.main()
