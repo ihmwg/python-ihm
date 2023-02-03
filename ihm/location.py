bdacf8aa07f392fac870e8c8de21bd8915e61a7a
Brinda Vallat <brinda.vallat@rcsb.org>
2023-02-03 13:34:07 -0500

    Add jPOSTrepo to location.py

--- ihm/location.py
+++ ihm/location.py
@@ -163,7 +163,15 @@ class PRIDELocation(DatabaseLocation):
     def __init__(self, db_code, version=None, details=None):
         super(PRIDELocation, self).__init__(self._db_name, db_code, version,
                                             details)
+class JPOSTLocation(DatabaseLocation):
+    """Something stored in the JPOST database.
+       See :class:`DatabaseLocation` for a description of the parameters
+       and :class:`Location` for discussion of the usage of these objects."""
+    _db_name = 'jPOSTrepo'
 
+    def __init__(self, db_code, version=None, details=None):
+        super(JPOSTLocation, self).__init__(self._db_name, db_code, version,
+                                            details)
 
 class BioGRIDLocation(DatabaseLocation):
     """Something stored in the BioGRID database.
