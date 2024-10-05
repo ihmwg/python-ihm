736edd24e256b0df468a26a29b2df52242008c61
Ben Webb <ben@salilab.org>
2024-10-04 22:33:09 -0700

    Clarify IDMapper for adding-to-system

--- ihm/reader.py
+++ ihm/reader.py
@@ -90,6 +90,9 @@ class IDMapper(object):
         self._cls = cls
         self._cls_args = cls_args
         self._cls_keys = cls_keys
+        # Fill in any existing IDs if available, so that we can objects
+        # to an existing system
+        # todo: handle objects where system_list is None
         # e.g. some handlers use FLRListAdapter, which doesn't
         # support iteration
         if system_list and hasattr(system_list, '__iter__'):
