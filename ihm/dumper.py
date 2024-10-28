6bb50f3acfac6cd665faad9885dacbcc8659c541
Ben Webb <ben@salilab.org>
2024-10-28 16:35:39 -0700

    Include group ID even if the group is empty

--- ihm/dumper.py
+++ ihm/dumper.py
@@ -1978,8 +1978,8 @@ class _EnsembleDumper(Dumper):
                 lp.write(ensemble_id=e._id, ensemble_name=e.name,
                          post_process_id=e.post_process._id if e.post_process
                          else None,
-                         model_group_id=e.model_group._id if e.model_group
-                         else None,
+                         model_group_id=e.model_group._id
+                         if e.model_group is not None else None,
                          ensemble_clustering_method=e.clustering_method,
                          ensemble_clustering_feature=e.clustering_feature,
                          num_ensemble_models=e.num_models,
